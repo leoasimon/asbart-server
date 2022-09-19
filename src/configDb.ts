c4bb9bc7dbe529c569382c9f242b5709c6763f37
lsimon <leo@lsimon.org>
2022-09-19 13:05:22 +0200

    db: trying heroku postgres

--- src/configDb.ts
+++ src/configDb.ts
@@ -2,15 +2,31 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Project } from './projects/project.entity';
 
 export const getConfig = (): TypeOrmModuleOptions => {
-  require('dotenv').config();
+  const env = process.env.NODE_ENV || 'development';
+
+  const common: Partial<TypeOrmModuleOptions> = {
+    type: 'postgres',
+    entities: [Project],
+  };
+
+  if (env === 'development') {
+    require('dotenv').config();
+
+    return {
+      ...common,
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DATABASE,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTRGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+    };
+  }
 
   return {
-    type: 'postgres',
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    port: Number(process.env.POSTGRES_PORT),
-    username: process.env.POSTRGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    entities: [Project],
+    ...common,
+    url: process.env.DATABASE_URL,
+    extra: {
+      ssl: true,
+    },
   };
 };
